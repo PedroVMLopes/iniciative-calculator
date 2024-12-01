9ffaab27739d4df5ad0a7bbc2ce66f05e72832d5
PedroVMLopes <pedrovitormlopes@gmail.com>
2024-12-01 16:48:29 -0300

    Retração do form de adicionar inimigo quando clicado no botão de adicionar outro ao grupo

--- src/components/Main/MenuAddInimigo/FormAddInimigo.js
+++ src/components/Main/MenuAddInimigo/FormAddInimigo.js
@@ -1,67 +1,71 @@
 import './MenuAddInimigo.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import { FormAddInimigoContext } from './MenuAddInimigo';
 
-export default function FormAddInimigo() {
+function FormAddInimigo() {
 
-    const [isExpanded, setIsExpanded] = useState(true);
+    const { isExpanded, setIsExpanded } = React.useContext(FormAddInimigoContext);
 
     return (
         
-        <div className="menu-add-inimigo menu-add-inimigo-corpo">
+            <div className="menu-add-inimigo menu-add-inimigo-corpo">
 
-            {!isExpanded && (
-                <div className="menu-add-inimigo inimigo-adicionado">
-                    <h1>Inimigo Criado</h1>
-                </div>
-            )}
+                {!isExpanded && (
+                    <div className="menu-add-inimigo inimigo-adicionado">
+                        <h1>Inimigo Criado</h1>
+                    </div>
+                )}
 
-            {isExpanded && (
-                <form id='add-inimigo'>
-                <label htmlFor="nome">Nome:</label>
-                <input 
-                    type="text" 
-                    id="nome" 
-                    name="nome" 
-                />
-                <br />
-                <label htmlFor="ca">CA:</label>
-                <input 
-                    type="number"
-                    id="ca" 
-                    name="ca" 
-                />
-                <br />
-                <label htmlFor="pv">PV:</label>
-                <input 
-                    type="number" 
-                    id="pv" 
-                    name="pv" 
-                />
-                <br />
-                <label htmlFor="mod">Mod:</label>
-                <input 
-                    type="number" 
-                    id="mod" 
-                    name="mod" 
-                />
-                <br />
-                <label htmlFor="rolagem">Rolagem:</label>
-                <input 
-                    type="text" 
-                    id="rolagem" 
-                    name="rolagem" 
-                />
-                <br />
-                <label htmlFor="condicao">Condição:</label>
-                <input 
-                    type="text" 
-                    id="condicao" 
-                    name="condicao" 
-                />
-                <br />
-                </form>
-            )}
-             
-        </div>
+                {isExpanded && (
+                    <form id='add-inimigo'>
+                    <label htmlFor="nome">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="nome" 
+                        name="nome" 
+                    />
+                    <br />
+                    <label htmlFor="ca">CA:</label>
+                    <input 
+                        type="number"
+                        id="ca" 
+                        name="ca" 
+                    />
+                    <br />
+                    <label htmlFor="pv">PV:</label>
+                    <input 
+                        type="number" 
+                        id="pv" 
+                        name="pv" 
+                    />
+                    <br />
+                    <label htmlFor="mod">Mod:</label>
+                    <input 
+                        type="number" 
+                        id="mod" 
+                        name="mod" 
+                    />
+                    <br />
+                    <label htmlFor="rolagem">Rolagem:</label>
+                    <input 
+                        type="text" 
+                        id="rolagem" 
+                        name="rolagem" 
+                    />
+                    <br />
+                    <label htmlFor="condicao">Condição:</label>
+                    <input 
+                        type="text" 
+                        id="condicao" 
+                        name="condicao" 
+                    />
+                    <br />
+                    </form>
+                )}
+                
+            </div>
     );
+    
 }
+
+export default FormAddInimigo;
--- src/components/Main/MenuAddInimigo/MenuAddInimigo.js
+++ src/components/Main/MenuAddInimigo/MenuAddInimigo.js
@@ -1,25 +1,32 @@
 import './MenuAddInimigo.css';
 import FormAddInimigo from './FormAddInimigo';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-export default function MenuAddInimigo() {
+export const FormAddInimigoContext = createContext();
 
-    const [isExpanded, setIsExpanded] = useState();
+function MenuAddInimigo() {
 
+    const [isExpanded, setIsExpanded] = useState(true);
+    
     const adicionarAoGrupo = (event) => {
         event.preventDefault();
-        setIsExpanded(false);
+        setIsExpanded( prevState => !prevState);
     }
 
     return (
-        <div className="menu-add-inimigo">
-            <div className="menu-add-inimigo header">
-                <h1>Adicionar Inimigo</h1>
+        <FormAddInimigoContext.Provider value={{ isExpanded, setIsExpanded }}>
+            <div className="menu-add-inimigo">
+                <div className="menu-add-inimigo header">
+                    <h1>Adicionar Inimigo</h1>
+                </div>
+                <FormAddInimigo />
+                <button className='botao-add-inimigo' type="submit" onClick={adicionarAoGrupo}>Adicionar outro ao grupo</button>
+                <button className='botao-add-inimigo' type="submit">Enviar</button>
             </div>
-            {isExpanded && <FormAddInimigo />}
-            <button className='botao-add-inimigo' type="submit" onClick={adicionarAoGrupo}>Adicionar outro ao grupo</button>
-            <button className='botao-add-inimigo' type="submit">Enviar</button>
-        </div>
+        </FormAddInimigoContext.Provider>
+        
     )
 
 }
+
+export default MenuAddInimigo;
