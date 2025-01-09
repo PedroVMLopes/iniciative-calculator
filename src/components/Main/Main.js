410d06360c71b1104d5b6a8c1ee80a41f281953f
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-01-09 17:10:54 -0300

    Implementação do Tailwind CSS

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,12 @@
 import BotoesAdicionar from "./BotoesAdicionar/BotoesAdicionar";
 
 export default function Main() {
-    return (
-        <div className="main">
-            <BotoesAdicionar />
-        </div>
-    );
+  return (
+    <div className="main">
+      <BotoesAdicionar />
+      <div className="bg-blue-500 text-white p4 text-center">
+        <h1 className="text-2xl font-bold">Teste Tailwind</h1>
+      </div>
+    </div>
+  );
 }
