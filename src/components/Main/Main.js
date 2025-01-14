03421dfef72fd6ad5b3b53187ed10ae70c48b8b0
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-01-14 18:11:38 -0300

    Customização da navbar

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,6 +7,7 @@ import {
   Link,
   useLocation,
 } from "react-router-dom";
+import { SiDungeonsanddragons } from "react-icons/si";
 
 const TestsContent = () => <Tests />;
 
@@ -29,29 +30,41 @@ const NavBar = () => {
 
   return (
     <nav
-      className="relative flex bg-neutral-600 bg-opacity-20 rounded-md p-1 justify-evenly pb-2 max-w-3xl text-[var(--bege)] font-bold shadow-xl"
+      className="relative flex rounded-md justify-evenly p-1 pt-2 max-w-3xl text-[var(--bege)] font-bold "
       id="navbar"
     >
       {links.map((link) => (
-        <Link key={link.path} to={link.path} className={`p-2 mx-2 `}>
+        <Link key={link.path} to={link.path} className={`p-2 mx-2 mt-1 `}>
           {link.label}
         </Link>
       ))}
-      {/* Barra de destaque */}
+
+      {/* Barra de destaque de baixo */}
+      {/*
       <div
-        className={`absolute bottom-0 h-1 bg-[var(--amarelo)] transition-all duration-300 rounded-md`}
+        className={`absolute h-1 bottom-0 bg-[var(--amarelo)] bg-opacity-20 transition-all duration-300 rounded-md`}
         style={{
           width: `${100 / links.length}%`,
           left: `${
             (links.findIndex((l) => l.path === location.pathname) || 0) *
             (100 / links.length)
           }%`,
-          backgroundImage: "../Decoracao_navbar.png",
-          backgroundSize: "80px",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
         }}
-      />
+      ></div>
+      */}
+
+      <div
+        className={`absolute ml-[30px] top-0 text-[var(--amarelo)] transition-all duration-300 rounded-md`}
+        style={{
+          width: `${100 / links.length}%`,
+          left: `${
+            (links.findIndex((l) => l.path === location.pathname) || 0) *
+            (100 / links.length)
+          }%`,
+        }}
+      >
+        <SiDungeonsanddragons />
+      </div>
     </nav>
   );
 };
