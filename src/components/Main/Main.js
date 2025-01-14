14c287b555d7e2588f506aaa33de4fb723298eb2
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-01-14 17:46:59 -0300

    Inserção da navbar funcional

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,77 @@
 import React from "react";
 import Tests from "./Tests/Tests";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from "react-router-dom";
 
-export default function Main() {
+const TestsContent = () => <Tests />;
+
+const NavBar = () => {
+  const location = useLocation();
+  const links = [
+    {
+      label: "Testes",
+      path: "/",
+    },
+    {
+      label: "Nomes",
+      path: "/names",
+    },
+    {
+      label: "NPC's",
+      path: "/npcs",
+    },
+  ];
+
   return (
-    <div className="Main flex flex-col items-center w-full xl:pl-[32%] lg:pl-[37%] sm:pl-[42%]  p-3">
-      <div className="NavBar flex bg-[var(--cinza-medio)] rounded-2xl p-1 justify-evenly max-w-3xl text-[var(--bege)] font-bold shadow-xl">
-        <button className="p-2 mx-2">Testes</button>
-        <button className="p-2 mx-2">Jogadores</button>
-        <button className="p-2 mx-2">Criador de NPC</button>
-      </div>
-      <div className="Content flex justify-center mt-3 w-[98%]">
-        <Tests />
-      </div>
-    </div>
+    <nav
+      className="relative flex bg-neutral-600 bg-opacity-20 rounded-md p-1 justify-evenly pb-2 max-w-3xl text-[var(--bege)] font-bold shadow-xl"
+      id="navbar"
+    >
+      {links.map((link) => (
+        <Link key={link.path} to={link.path} className={`p-2 mx-2 `}>
+          {link.label}
+        </Link>
+      ))}
+      {/* Barra de destaque */}
+      <div
+        className={`absolute bottom-0 h-1 bg-[var(--amarelo)] transition-all duration-300 rounded-md`}
+        style={{
+          width: `${100 / links.length}%`,
+          left: `${
+            (links.findIndex((l) => l.path === location.pathname) || 0) *
+            (100 / links.length)
+          }%`,
+          backgroundImage: "../Decoracao_navbar.png",
+          backgroundSize: "80px",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+    </nav>
   );
-}
+};
+
+const Main = () => {
+  return (
+    <Router>
+      <div
+        className="flex flex-col items-center w-full xl:pl-[32%] lg:pl-[37%] sm:pl-[42%]  p-3"
+        id="main"
+      >
+        <NavBar />
+        <div className="flex justify-center mt-3 w-[98%]" id="content">
+          <Routes>
+            <Route path="/" element={<TestsContent />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default Main;
