9b4ab2f8bf14d45ae8324dcb099f22c542d70d95
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-01-14 22:14:19 -0300

    A página de Party gera iniciativas aleatórias para todos

--- src/components/Main/Party/Party.js
+++ src/components/Main/Party/Party.js
@@ -10,19 +10,28 @@ const Party = () => {
     localStorage.setItem("players", JSON.stringify(players));
   };
 
-  const cardPlayer = (player) => {
-    const iniciativa = player.iniciativa;
+  const CardPlayer = (player) => {
     const nome = player.dados[0].nome;
     const ca = player.dados[0].ca;
     const pv = player.dados[0].pv;
     const mod = player.dados[0].mod;
     const roll = player.dados[0].rolagem;
     const id = player.dados[0].id;
+    const iniciativa = Number(roll) + Number(mod);
+
+    useEffect(() => {
+      player.iniciativa = Math.floor(Math.random() * 20) + 1 + Number(mod);
+    }, [player.dados[0].rolagem]);
+
+    console.log(player);
+
+    /* const iniciativa = Math.floor(Math.random() * 20) + 1 + Number(mod); */
+
     return (
       <div className="mx-3 px-2 max-w-80" key={id}>
         <div className="flex flex-row items-center justify-center bg-[var(--azul-claro)] rounded-sm p-3 font-bold text-[var(--bege)]">
           <h2 className="font-bold font-cormorant text-lg">{nome}</h2>
-          <h3 className="font-greatVibes text-lg pl-3">{iniciativa}</h3>
+          <p className="ml-2 font-thin">{iniciativa}</p>
         </div>
         <div className="grid grid-cols-2 gap-1 bg-[var(--cinza-claro)] rounded-sm p-3">
           <div className="flex flex-row justify-around p-2">
@@ -58,7 +67,7 @@ const Party = () => {
               type="text"
               name="playerRoll"
               className="w-full ml-2 pl-3 rounded-md font-bold"
-              value={roll}
+              onChange={(e) => (player.dados[0].rolagem = e.target.value)}
             />
           </div>
         </div>
@@ -71,15 +80,22 @@ const Party = () => {
       <h1 className="font-bold font-unifraktur text-3xl pt-4 text-white">
         Grupo De Jogadores
       </h1>
-      <button
-        className="mt-6 bg-[var(--azul-claro)] hover:bg-[var(--azul-escuro)] rounded-sm p-3  font-bold text-[var(--bege)] "
-        id="rollInitiative"
-        onClick={() => savePlayer(players)}
-      >
-        Rolar Iniciativa Do Grupo
-      </button>
+      <div className="flex flex-col items-center">
+        <button
+          className="peer mt-6 bg-[var(--azul-claro)] hover:bg-[var(--azul-escuro)] rounded-sm p-3  font-bold text-[var(--bege)] "
+          id="rollInitiative"
+          onClick={() => savePlayer(players)}
+        >
+          Rolar Iniciativa Do Grupo
+        </button>
+        <p className="hidden peer-hover:opacity-100 peer-hover:block transition-all duration-300 ease-in-out text-sm text-white rounded-sm p-3">
+          Esse botão utiliza os dados mostrados abaixo (salvos previamente) para
+          rolar as iniciativas dos jogadores.
+        </p>
+      </div>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  gap-4 pt-12">
-        {players.map((player) => cardPlayer(player))}
+        {players.map((player) => CardPlayer(player))}
       </div>
     </div>
   );
