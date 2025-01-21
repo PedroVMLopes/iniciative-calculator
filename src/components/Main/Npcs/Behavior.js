683c856e55694d9605127d4170fc0a4066b83152
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-01-20 23:52:08 -0300

    Botões de randomizar e deletar seleções dos Traits funcionando

--- src/components/Main/Npcs/Behavior.js
+++ src/components/Main/Npcs/Behavior.js
@@ -1,34 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { FaFeatherAlt } from "react-icons/fa";
+import { FaEraser } from "react-icons/fa";
 
 export function Behavior() {
-  return (
-    <div className="flex flex-col justify-center items-center mt-6 w-full bg-[var(--cinza-medio)] rounded-md p-1">
-      <div className="flex flex-col justify-center items-center w-full bg-[var(--cinza-escuro)] rounded-md">
-        <div className="flex flex-row items-center w-full bg-[var(--cinza-escuro)] rounded-t-md p-6 pt-4">
-          <h1 className="justify-start font-greatVibes text-3xl text-[var(--bege)]">
-            Traços de Comportamento{" "}
-          </h1>
-          <p className="px-2">-</p>
-          <h2 className="text-lg">
-            Dita como será a interação dele com os jogadores
-          </h2>
-        </div>
-        <div className="flex flex-row justify-evenly w-full">
-          <Traits />
-        </div>
-        <div className="flex flex-row justify-end w-full">
-          <button className="animate-pulse p-2 m-2 rounded-md bg-[var(--cinza-escuro)] hover:bg-[var(--laranja)] shadow-xl">
-            Selecionar Aleatóriamente
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const Traits = () => {
   const [behaviors, setBehaviors] = useState([]);
+
   const [selectedTraits, setSelectedTraits] = useState({
     positive: "",
     negative: "",
@@ -42,10 +18,56 @@ const Traits = () => {
       .catch((error) => console.error("Erro ao buscar behaviors", error));
   }, []);
 
-  useEffect(() => {
-    console.log("selectedTraits:", selectedTraits.negative);
-  }, [selectedTraits]);
+  const selectRandomTraits = () => {
+    const newSelectedTraits = {};
+    for (const [key, value] of Object.entries(behaviors)) {
+      newSelectedTraits[key] =
+        value.traits[Math.floor(Math.random() * value.traits.length)];
+    }
+    setSelectedTraits(newSelectedTraits);
+  };
 
+  return (
+    <div className="flex flex-col justify-center items-center mt-6 w-full bg-[var(--cinza-medio)] rounded-md p-1">
+      <div className="flex flex-col justify-center items-center w-full bg-[var(--cinza-escuro)] rounded-md">
+        <div className="flex flex-row items-center w-full bg-[var(--cinza-escuro)] rounded-t-md p-6 pt-4">
+          <h1 className="justify-start font-greatVibes text-3xl text-[var(--bege)]">
+            Traços de Comportamento{" "}
+          </h1>
+          <p className="px-2">-</p>
+          <h2 className="text-lg">
+            Dita como será a interação dele com os jogadores
+          </h2>
+        </div>
+        <div className="flex flex-row justify-evenly w-full">
+          <Traits
+            behaviors={behaviors}
+            selectedTraits={selectedTraits}
+            setSelectedTraits={setSelectedTraits}
+          />
+        </div>
+        <div className="flex flex-row justify-end w-full">
+          <button
+            className="animate-pulse p-2 m-2 mr-1 rounded-md bg-[var(--cinza-escuro)] hover:bg-[var(--laranja)] shadow-xl"
+            onClick={selectRandomTraits}
+          >
+            Selecionar Aleatoriamente
+          </button>
+          <button
+            className="animate-pulse p-2 m-2 ml-0 rounded-md bg-[var(--cinza-escuro)] hover:bg-[var(--laranja)] shadow-xl"
+            onClick={() =>
+              setSelectedTraits({ positive: "", negative: "", neutral: "" })
+            }
+          >
+            <FaEraser />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Traits = ({ behaviors, selectedTraits, setSelectedTraits }) => {
   const handleSelection = (key, trait) => {
     setSelectedTraits((prevState) => ({
       ...prevState,
@@ -57,7 +79,7 @@ const Traits = () => {
     <div className="flex flex-row justify-evenly w-full bg-[var(--cinza-escuro)] rounded-b-md p-3">
       {Object.entries(behaviors).map(([key, value]) => (
         <div key={key} className="flex flex-col items-center rounded-md">
-          <h1 className=" font-greatVibes text-3xl ">{value.title}</h1>
+          <h1 className="font-greatVibes text-3xl">{value.title}</h1>
           <div className="flex flex-col justify-center">
             {value.traits.map((trait) => (
               <label
@@ -67,7 +89,7 @@ const Traits = () => {
                 <input
                   type="radio"
                   className="hidden"
-                  name="key"
+                  name={key}
                   value={trait}
                   checked={selectedTraits[key] === trait}
                   onChange={() => handleSelection(key, trait)}
