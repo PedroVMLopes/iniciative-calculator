3d4958e09806a4897da797d429834d2c865cad9c
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-02-11 00:58:13 -0300

    Alterações no código do Electron. A aplicação é funcional no ambiente de desenvolvimento

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const url = require("url");
 
 function createWindow() {
-  console.log("NODE_ENV:", process.env.NODE_ENV); // Para depurar
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
   });
-
-  win.webContents.openDevTools();
 
   if (process.env.NODE_ENV === "development") {
     win.loadURL("http://localhost:3000");
   } else {
-    win.loadFile(path.join(__dirname, "build", "index.html"));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "build", "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    );
   }
 }
 
 app.whenReady().then(createWindow);
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
